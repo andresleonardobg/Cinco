7df63f7bef769aa4b78fbfcbee735c7ed82ed5a9
Andres Leonardo <andresleonardobg@gmail.com>
2022-03-20 23:16:40 -0500

    firts version finished

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -23,13 +23,31 @@ var taller
 var vis = true
 var obj = false
 
+
+var music = [AudioStreamPlayer.new(), AudioStreamPlayer.new(), AudioStreamPlayer.new()]
+var ambient = load("res://assets/music/ambient.mp3")
+var ambient_two = load("res://assets/music/ambient-2.mp3")
+var ambient_three = load("res://assets/music/ambient-3.mp3")
+
+
 func show_dialog(t, node):
 	var d = dialog.instance()
 	d.textSelected = t
 	node.add_child(d)
 
 func _ready() -> void:
+	
+	_add_music(music[0], ambient, -16)
+	_add_music(music[1], ambient_two, -80)
+	_add_music(music[2], ambient_three, -80)
 	pass
+
+func _add_music(m,s, v) -> void:
+	m.set_stream(s)
+	m.volume_db = v
+	m.pitch_scale = 1
+	m.play()
+	add_child(m)
 
 func _process(_delta: float) -> void:
 	#dialogos
@@ -37,6 +55,12 @@ func _process(_delta: float) -> void:
 		if map:
 			node_level = get_node("/root/level/CanvasLayer")
 			taller = get_node("/root/level/map/navigation/door7")
+			
+			if map.y == -4000:
+				get_child(1).volume_db = 1
+				
+			if map.y == -4000 and map.x == -1200:
+				get_child(2).volume_db = 1
 		#cuando se encuentra por primera vez al viejo
 			if map.y == -4000 and map.x == 0 and firts_dialog:
 				show_dialog(1, node_level)
--- scripts/final_dialog.gd
+++ scripts/final_dialog.gd
@@ -2,10 +2,23 @@ extends Node2D
 
 var dialogs = load("res://scenes/dialogs.tscn")
 var verificar = false
+var lenguage = [
+	"Hecho con\n\nGodot Engine\nKrita\nLMMS\n\nGracias por jugarlo",
+	"Made with\n\nGodot Engine\nKrita\nLMMS\n\nthanks for playing it"
+]
+
+var count = 0
 
 func _ready() -> void:
 	$ColorRect/AnimationPlayer.play("fadeIn")
-	pass
+	$ColorRect4/Label.text = lenguage[Global.lenguage]
+
+func _input(event: InputEvent) -> void:
+
+	if Input.is_action_pressed("click"):
+		count += 1
+	
+	
 
 func dialog():
 	var d = dialogs.instance()
@@ -20,6 +33,9 @@ func _on_AnimationPlayer_animation_finished(_anim_name: String) -> void:
 
 func _process(_delta: float) -> void:
 	
+	if count == 4:
+		$AnimatedSprite.set_animation("fall")
+	
 	if verificar:
 		if !self.has_node("Dialog"):
 			$ColorRect2.visible = true
@@ -27,4 +43,4 @@ func _process(_delta: float) -> void:
 
 
 func _on_AnimationPlayer2_animation_finished(_anim_name: String) -> void:
-	get_tree().change_scene("res://scenes/final_scene.tscn")
+	$ColorRect4.visible = true
